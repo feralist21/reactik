cdb947a54985ee830e4c6f2a1205736caa11c4b0
Alexandr Dementev <ad@adictgroup.ru>
2024-08-30 11:26:17 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div className='container mx-auto py-2'>
+            <h1 className='text-3xl font-bold underline'>Hello Pokemaniac!</h1>
+        </div>
+    );
+};
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@ui': path.resolve(__dirname, 'src/shared/ui'),
+            '@assets': path.resolve(__dirname, 'src/assets/'),
+        },
+    },
+    plugins: [react()],
+});
